4ef65a9a40329fe76222c414fd49e72d36783e25
Mikhel V Kuttickal <81845280+Mikhelmariya@users.noreply.github.com>
2024-02-22 23:38:58 +0530

    basic logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,36 @@
+import 'package:flame/events.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // To ensure initialization of instance
+  Flame.device.fullScreen(); // To make the game full screen
   runApp(GameWidget(game: MyGame()));
 }
 
-class MyGame extends FlameGame{
-  
+class MyGame extends FlameGame with TapDetector {
+  // Adding the game execution loop
+
+  @override
+  Future<void> onLoad() async {
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+  }
+
+  @override
+  void onTapUp(TapUpInfo info) {
+    print('x: ${info.eventPosition.global.x}, ');
+    print('y: ${info.eventPosition.global.y}, ');
+  }
 }
